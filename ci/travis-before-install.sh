ae479c9ff9fe4aaa5935e0153a434f590b1ad0fe
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2017-06-18 20:21:04 -0300

    Fix OSX binaries

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -3,8 +3,18 @@
 echo "### 1. getting batari  basic"
 echo "# 1.1 downloading"
 wget http://bataribasic.com/batari_Basic_version_1.0.zip
-wget http://bataribasic.com/bB_Linux_binaries_version_1.0.zip
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  wget http://bataribasic.com/bB_Linux_binaries_version_1.0.zip
+else 
+  wget http://bataribasic.com/bB_OSX_binaries_version_1.0.zip
+fi
 
 echo "# 1.1 unzipping"
 unzip -o batari_Basic_version_1.0.zip
-unzip -o bB_Linux_binaries_version_1.0.zip
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  unzip -o bB_Linux_binaries_version_1.0.zip
+else
+  unzip -o bB_OSX_binaries_version_1.0.zip
+fi
