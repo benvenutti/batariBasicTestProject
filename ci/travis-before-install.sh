7e996576a537aa84745bd3415cd8ed632860ad8d
Diogo Benvenutti <diogo.a.benvenutti@gmail.com>
2017-06-19 19:07:40 -0300

    Update script for OSX

--- ci/travis-before-install.sh
+++ ci/travis-before-install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-echo "### 1. getting batari  basic"
+echo "### 1. getting batari basic"
 echo "# 1.1 downloading"
-wget http://bataribasic.com/batari_Basic_version_1.0.zip
-wget http://bataribasic.com/bB_Linux_binaries_version_1.0.zip
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  wget http://bataribasic.com/batari_Basic_version_1.0.zip
+  wget http://bataribasic.com/bB_Linux_binaries_version_1.0.zip
+else
+  wget http://7800.8bitdev.org/images/1/18/BB.1.1d.reveng40.zip
+fi
 
 echo "# 1.1 unzipping"
-unzip -o batari_Basic_version_1.0.zip
-unzip -o bB_Linux_binaries_version_1.0.zip
+
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+  unzip -o batari_Basic_version_1.0.zip
+  unzip -o bB_Linux_binaries_version_1.0.zip
+else
+  unzip -o BB.1.1d.reveng40.zip
+  mv bB.1.1d.reveng40/* .
+  ls
+fi
