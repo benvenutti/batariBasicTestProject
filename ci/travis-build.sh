00cd53a871d295b79d707cacf62dfaa5b444a8d3
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2017-07-15 19:29:05 -0300

    Change bb compiler to 1.1

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -1,9 +1,62 @@
-#!/bin/bash
+#!/bin/sh
 
-cd build
+# This file is an (almost) exact copy from the build bash script from 
+# http://7800.8bitdev.org/index.php/Batari_basic. The only thing that 
+# was changed was the exit constant when the compilation and the 
+# assembly failed. This was done so the CI system is able to tell that 
+# something was wrong with the build.
 
-if [ "$TRAVIS_OS_NAME" == "linux" ]; then 
-  ../ci/travis-build-linux.sh sample.bas 
-else 
-  ../ci/travis-build-osx.sh sample.bas 
+# do some quick sanity checking...
+
+if [ ! "$bB" ] ; then
+  echo "### WARNING: the bB envionronment variable isn't set."
 fi
+
+OSTYPE=$(uname -s)
+ARCH=$(uname -m)
+for EXT in "" .$OSTYPE.x86 .$OSTYPE.$ARCH .$OSTYPE ; do
+  echo | preprocess$EXT 2>/dev/null >&2 && break
+done
+
+echo | preprocess$EXT 2>/dev/null >&2 && break
+if [ ! $? = 0 ] ; then
+  echo "### ERROR: couldn't find bB binaries for $OSTYPE($ARCH). Exiting."
+  exit 1
+fi
+
+#do dasm separately, because it's distributed separately
+for DASMEXT in "" .$OSTYPE.x86 .$OSTYPE.$ARCH .$OSTYPE ; do
+  dasm$DASMEXT 2>/dev/null >&2 
+  [ $? = 1 ] && break
+done
+dasm$DASMEXT 2>/dev/null >&2 
+if [ ! $? = 1 ] ; then
+  echo "### ERROR: couldn't find dasm binary for $OSTYPE($ARCH). Exiting."
+  exit 1
+fi
+
+DV=$(dasm$DASMEXT 2>/dev/null | grep ^DASM | head -n1)
+echo "Found dasm version: $DV"
+  
+echo "Starting build of $1"
+preprocess$EXT<$1 | 2600basic$EXT -i "$bB" >bB.asm
+#preprocess$EXT<$1 | valgrind --leak-check=yes 2600basic$EXT -i "$bB" >bB.asm
+if [ "$?" -ne "0" ]
+ then
+  echo "Compilation failed."
+  exit 1
+fi
+if [ "$2" = "-O" ]
+  then
+   postprocess$EXT -i "$bB" | optimize$EXT>$1.asm
+  else
+   postprocess$EXT -i "$bB" >$1.asm
+fi
+dasm$DASMEXT $1.asm -I"$bB/includes" -f3 -l$1.list.txt -s$1.symbol.txt -o$1.bin | bbfilter$EXT
+if [ "$?" -ne "0" ]
+ then
+  echo "Assembly failed."
+  exit 1
+fi
+echo "Build complete."
+exit
